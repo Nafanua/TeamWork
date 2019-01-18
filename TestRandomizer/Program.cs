7b6dcf7cfa5e8ca1382a1aa43785303bd511da2a
Горожанкин Станислав <sgorozhankin@gmail.com>
2019-01-18 21:30:50 +0200

    add two thread in randomizer

---
+++ TestRandomizer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestRandomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Randomizer().Start();
+        }
+
+        class Randomizer
+        {
+
+            public void Start()
+            {
+                var ThreadBuy = new Thread(Buy);
+                var ThreadSell = new Thread(Sell);
+                ThreadBuy.Start();
+                ThreadSell.Start();
+
+
+                ConsoleKeyInfo key;
+                do
+                {
+                    key = Console.ReadKey();
+
+                    if (key.Key != ConsoleKey.Escape) continue;
+                    ThreadBuy.Interrupt();
+                    ThreadSell.Interrupt();
+                    Console.WriteLine("Stopped");
+                } while (key.Key != ConsoleKey.Escape);
+
+            }
+
+            private static void Buy()
+            {
+                var rnd = new Random();
+                while (true)
+                {
+                    Console.WriteLine("Buy");
+                    Thread.Sleep(rnd.Next(15000));
+                }
+            }
+
+            private static void Sell()
+            {
+                var rnd = new Random();
+                while (true)
+                {
+                    Console.WriteLine("Sell");
+                    Thread.Sleep(rnd.Next(15000));
+                }
+            }
+        }
+    }
+}
