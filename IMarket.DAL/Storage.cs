1ea6e7556333fd25cfc26726d291b231e353cbc2
Nafanua <serjoid2601@hotmail.com>
2019-01-18 16:15:12 +0200

    Added storage logic

--- IMarket.DAL/Storage.cs
+++ IMarket.DAL/Storage.cs
@@ -1,10 +1,46 @@
-﻿using IMarket.Models.Models;
+﻿using System;
+using IMarket.Models.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IMarket.DAL
 {
     public static class Storage
     {
-        public static IEnumerable<ItemBase> Stock = new List<ItemBase>();
+        private const double MaximumStorageCapacity = 1000;
+
+        private static readonly List<ItemBase> Stock = new List<ItemBase>();
+
+        public static double GetStorageCapacity()
+        {
+            return Stock.Sum(i => i.Weight);
+        }
+
+        public static bool AddToStorage(ItemBase item)
+        {
+            if (GetStorageCapacity() >= MaximumStorageCapacity)
+            {
+                return false;
+            }
+
+            Stock.Add(item);
+
+            return true;
+        }
+
+        public static IEnumerable<ItemBase> GetAll()
+        {
+            return Stock.GetRange(0, Stock.Count);
+        }
+
+        public static IEnumerable<ItemBase> GetByName(string name)
+        {
+            return Stock.Where(i => i.Name.Equals(name));
+        }
+
+        public static IEnumerable<ItemBase> GetMany(Func<ItemBase, bool> filter)
+        {
+            return Stock.Where(filter);
+        }
     }
 }
