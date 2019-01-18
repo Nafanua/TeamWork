9e79970ada2e4e63234d5ae7d8e2eb4b4ad5465b
Nafanua <serjoid2601@hotmail.com>
2019-01-18 12:11:54 +0200

    Init

---
+++ IMarket/IMarket/Models/ItemBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IMarket.Models.Enums;
+
+namespace IMarket.Models
+{
+    public class ItemBase
+    {
+        public string Name { get; set; }
+
+        public DateTime DeliveryTime { get; set; }
+
+        public DateTime SellingTime { get; set; }
+
+        public int Weight { get; set; }
+
+        public ItemType Type { get; set; }
+    }
+}
