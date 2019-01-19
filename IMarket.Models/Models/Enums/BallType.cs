012e7f4c21b4c6fb3805bb2fa565edbe1cf184a1
Nafanua <serjoid2601@hotmail.com>
2019-01-19 14:57:38 +0200

    Revert "try toresolve merge problems"
    This reverts commit e2d9be55fba0dd589ff3d7874da52ed376f02cc2.

---
+++ IMarket.Models/Models/Enums/BallType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMarket.Models.Models.Enums
+{
+    public enum BallType
+    {
+        SoccerBall,
+        VolleyballBall,
+        BasketballBall,
+        BaseballBall
+    }
+}
---
+++ IMarket.Models/Models/Enums/ItemType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMarket.Models.Models.Enums
+{
+    public enum ItemType
+    {
+        Ball,
+        Clothes,
+        SportAccessories,
+        WinterSport
+    }
+}
