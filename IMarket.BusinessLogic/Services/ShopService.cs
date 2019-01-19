b63faa68071055359e993f11154118fb2c010dc0
Nafanua <serjoid2601@hotmail.com>
2019-01-19 15:24:55 +0200

    Add dev files

---
+++ IMarket.BusinessLogic/Services/ShopService.cs
@@ -0,0 +1,9 @@
+﻿using IMarket.BusinessLogic.Services.Abstracts;
+
+namespace IMarket.BusinessLogic.Services
+{
+    public class ShopService : IShopService
+    {
+
+    }
+}
---
+++ IMarket/Models/StockViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IMarket.Models.Models;
+
+namespace IMarket.Models
+{
+    public class StockViewModel
+    {
+        public IEnumerable<ItemBase> Items { get; set; }
+
+        public IEnumerable<ItemBase> ItemsOutOfStock { get; set; }
+
+        public IEnumerable<ItemBase> ItemsNotFound { get; set; }
+
+        public int ItemsCount { get; set; }
+    }
+}
