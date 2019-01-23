de693d2a722168d9310a74e2940fcf034e24c92b
Nafanua <serjoid2601@hotmail.com>
2019-01-23 21:05:22 +0200

    Revert "Merge pull request #64 from Nafanua/RemoveFieldQuantity"
    This reverts commit ea1d93f2deab9b12675ba6d6a772438a05f77444, reversing
    changes made to da034d6cb23eb29a69440103b6efaafbf7419271.

--- IMarket.DAL/Storage.cs
+++ IMarket.DAL/Storage.cs
@@ -14,86 +14,86 @@ namespace IMarket.DAL
         private static readonly List<ItemBase> Stock = new List<ItemBase>() {
             // Clothes
             new ClothesModel(ConcreteType.TShirt){ConcreteType = ConcreteType.TShirt, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White TShirt",Size = "46 - 56", Type = GeneralType.Clothes},
+                Name = "White TShirt",Size = "46 - 56", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.TShirt){ConcreteType = ConcreteType.TShirt, Color = Color.Black, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Black TShirt",Size = "48 - 56", Type = GeneralType.Clothes},
+                Name = "Black TShirt",Size = "48 - 56", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.TShirt){ConcreteType = ConcreteType.TShirt, Color = Color.Cyan, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Blue TShirt",Size = "48 - 56", Type = GeneralType.Clothes},
+                Name = "Blue TShirt",Size = "48 - 56", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.Pants){ConcreteType = ConcreteType.Pants, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White Pants",Size = "46 - 56", Type = GeneralType.Clothes},
+                Name = "White Pants",Size = "46 - 56", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.Pants){ConcreteType = ConcreteType.Pants, Color = Color.Black, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Black Pants",Size = "48 - 56", Type = GeneralType.Clothes},
+                Name = "Black Pants",Size = "48 - 56", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.Pants){ConcreteType = ConcreteType.Pants, Color = Color.Blue, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Blue Pants",Size = "48 - 56", Type = GeneralType.Clothes},
+                Name = "Blue Pants",Size = "48 - 56", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.Shoes){ConcreteType = ConcreteType.Shoes, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White Shoes",Size = "37 - 45", Type = GeneralType.Clothes},
+                Name = "White Shoes",Size = "37 - 45", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.Shoes){ConcreteType = ConcreteType.Shoes, Color = Color.Red, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Red Shoes",Size = "39 - 46", Type = GeneralType.Clothes},
+                Name = "Red Shoes",Size = "39 - 46", Quantity = 10, Type = GeneralType.Clothes},
             new ClothesModel(ConcreteType.Shoes){ConcreteType = ConcreteType.Shoes, Color = Color.Black, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Black Shoes",Size = "39 - 46", Type = GeneralType.Clothes},
+                Name = "Black Shoes",Size = "39 - 46", Quantity = 10, Type = GeneralType.Clothes},
             // Ball
             new BallModel(ConcreteType.SoccerBall){ConcreteType = ConcreteType.SoccerBall, Color = Color.Red, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Red Ball",Diameter = 22, Type = GeneralType.Ball},
+                Name = "Red Ball",Diameter = 22, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.SoccerBall){ConcreteType = ConcreteType.SoccerBall, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White Ball",Diameter = 22, Type = GeneralType.Ball},
+                Name = "White Ball",Diameter = 22, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.VolleyballBall){ConcreteType = ConcreteType.VolleyballBall, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White Ball",Diameter = 19, Type = GeneralType.Ball},
+                Name = "White Ball",Diameter = 19, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.VolleyballBall){ConcreteType = ConcreteType.VolleyballBall, Color = Color.Black, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Grey Ball",Diameter = 19, Type = GeneralType.Ball},
+                Name = "Grey Ball",Diameter = 19, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.BasketballBall){ConcreteType = ConcreteType.BasketballBall, Color = Color.Blue, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Orange Ball",Diameter = 27, Type = GeneralType.Ball},
+                Name = "Orange Ball",Diameter = 27, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.BasketballBall){ConcreteType = ConcreteType.BasketballBall, Color = Color.Cyan, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Yellow&Purple Ball",Diameter = 27, Type = GeneralType.Ball},
+                Name = "Yellow&Purple Ball",Diameter = 27, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.BaseballBall){ConcreteType = ConcreteType.BaseballBall, Color = Color.Black, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Grey Ball",Diameter = 8, Type = GeneralType.Ball},
+                Name = "Grey Ball",Diameter = 8, Quantity = 10, Type = GeneralType.Ball},
             new BallModel(ConcreteType.BaseballBall){ConcreteType = ConcreteType.BaseballBall, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White Ball",Diameter = 8, Type = GeneralType.Ball},
+                Name = "White Ball",Diameter = 8, Quantity = 10, Type = GeneralType.Ball},
             // SportsAccessories
             new SportsAccessoriesModel(ConcreteType.Stopwatch){ConcreteType = ConcreteType.Stopwatch, Color = Color.Blue, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Blue Stopwatch", Type = GeneralType.SportAccessories},
+                Name = "Blue Stopwatch", Quantity = 10, Type = GeneralType.SportAccessories},
             new SportsAccessoriesModel(ConcreteType.Stopwatch){ConcreteType = ConcreteType.Stopwatch, Color = Color.Green, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Green Stopwatch", Type = GeneralType.SportAccessories},
+                Name = "Green Stopwatch", Quantity = 10, Type = GeneralType.SportAccessories},
             new SportsAccessoriesModel(ConcreteType.BallPump){ConcreteType = ConcreteType.BallPump, Color = Color.Green, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Green Pump", Type = GeneralType.SportAccessories},
+                Name = "Green Pump", Quantity = 10, Type = GeneralType.SportAccessories},
             new SportsAccessoriesModel(ConcreteType.BallPump){ConcreteType = ConcreteType.BallPump, Color = Color.Blue, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Blue Pump", Type = GeneralType.SportAccessories},
+                Name = "Blue Pump", Quantity = 10, Type = GeneralType.SportAccessories},
             new SportsAccessoriesModel(ConcreteType.Whistle){ConcreteType = ConcreteType.Whistle, Color = Color.Cyan, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Yellow Whistle", Type = GeneralType.SportAccessories},
+                Name = "Yellow Whistle", Quantity = 10, Type = GeneralType.SportAccessories},
             new SportsAccessoriesModel(ConcreteType.Whistle){ConcreteType = ConcreteType.Whistle, Color = Color.White, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "White Whistle", Type = GeneralType.SportAccessories},
+                Name = "White Whistle", Quantity = 10, Type = GeneralType.SportAccessories},
             // WinterSports
             // Лыжи
             new WinterSportsModel(ConcreteType.Skiing){ConcreteType = ConcreteType.Skiing, Color = Color.Red, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Red Skiing",Lenght = 1.5, Type = GeneralType.WinterSport},
+                Name = "Red Skiing",Lenght = 1.5, Quantity = 10, Type = GeneralType.WinterSport},
             new WinterSportsModel(ConcreteType.Skiing){ConcreteType = ConcreteType.Skiing, Color = Color.Blue, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Blue Skiing",Lenght = 1.5, Type = GeneralType.WinterSport},
+                Name = "Blue Skiing",Lenght = 1.5, Quantity = 10, Type = GeneralType.WinterSport},
             // Санки
             new WinterSportsModel(ConcreteType.Sled){ConcreteType = ConcreteType.Sled, Color = Color.Green, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Green Sled",Lenght = 0.8, Type = GeneralType.WinterSport},
+                Name = "Green Sled",Lenght = 0.8, Quantity = 10, Type = GeneralType.WinterSport},
             new WinterSportsModel(ConcreteType.Sled){ConcreteType = ConcreteType.Sled, Color = Color.Blue, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Blue Sled",Lenght = 0.8, Type = GeneralType.WinterSport},
+                Name = "Blue Sled",Lenght = 0.8, Quantity = 10, Type = GeneralType.WinterSport},
             // Клюшки
             new WinterSportsModel(ConcreteType.Sticks){ConcreteType = ConcreteType.Sticks, Color = Color.Black, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Yellow Sticks",Lenght = 1.9, Type = GeneralType.WinterSport},
+                Name = "Yellow Sticks",Lenght = 1.9, Quantity = 10, Type = GeneralType.WinterSport},
             new WinterSportsModel(ConcreteType.Sticks){ConcreteType = ConcreteType.Sticks, Color = Color.Red, DeliveryTime = new DateTime(2018, 11, 07),
-                Name = "Red Sticks",Lenght = 1.9, Type = GeneralType.WinterSport},
+                Name = "Red Sticks",Lenght = 1.9, Quantity = 10, Type = GeneralType.WinterSport},
         };
 
         private static readonly List<ItemBase> ItemNotFound = new List<ItemBase>
         {
             new ClothesModel(ConcreteType.TShirt){ConcreteType = ConcreteType.TShirt, Color = Color.White, DeliveryTime = new  DateTime(2018, 11, 08),
-                 Name = "ItemNotFound",Size = "46 - 56", Type = GeneralType.Clothes}
+                 Name = "ItemNotFound",Size = "46 - 56", Quantity = 1, Type = GeneralType.Clothes}
         };
 
         private static readonly List<ItemBase> ItemNoPlaceInStock = new List<ItemBase>
         {
             new WinterSportsModel(ConcreteType.Sled){ConcreteType = ConcreteType.Sled, Color = Color.White, DeliveryTime = new  DateTime(2018, 11, 07),
-                Name = "ItemNoPlaceInStock",Lenght = 0.8, Type = GeneralType.WinterSport}
+                Name = "ItemNoPlaceInStock",Lenght = 0.8, Quantity = 2, Type = GeneralType.WinterSport}
         };
 
         public static double GetStorageCapacity()
         {
-            return Stock.Sum(i => (i.Weight));
+            return Stock.Sum(i => (i.Weight * i.Quantity));
         }
 
         public static bool AddToStorage(ItemBase item)
@@ -163,6 +163,19 @@ namespace IMarket.DAL
                 }
             }
 
+            return false;
+        }
+
+        public static bool Sell(int index, int quantity)
+        {
+            lock (locker)
+            {
+                if (Stock[index].Quantity > quantity)
+                {
+                    Stock[index].Quantity -= quantity;
+                    return true;
+                }
+            }
             return false;
         }
 
